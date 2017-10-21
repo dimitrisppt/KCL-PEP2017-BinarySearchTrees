66f6cabd6f1ef14d63c0fa37ad4748f85c96ef1f
Dimitris Papatheodoulou <dimitris.papatheodoulou@kcl.ac.uk>
2017-10-21 16:34:52 +0100

    Implemented treenode.h, write function doesn't work properly.

--- treenode.h
+++ treenode.h
@@ -14,6 +14,48 @@ using std::unique_ptr;
 using std::pair;
 
 // TODO your code for the TreeNode class goes here:
+template<typename T>
+class TreeNode {
+
+  public:
+    T data;
+    unique_ptr<TreeNode<T>> leftChild;
+    unique_ptr<TreeNode<T>> rightChild;
+    TreeNode<T> * parent;
+
+  public:
+    TreeNode(const T & dataIn)
+        : data(dataIn), parent(nullptr) {
+
+    }
+
+
+
+  void setLeftChild(TreeNode<T>* child) {
+    leftChild.reset(child);
+    child->parent = this;
+  }
+
+  void setRightChild(TreeNode<T>* child) {
+    rightChild.reset(child);
+    child->parent = this;
+
+  }
+
+  ostream& write(ostream & os) const {
+
+      if (leftChild != nullptr) {
+        os << " " << this->leftChild->leftChild->data << " " << " " << leftChild->data << " " << " " << leftChild->parent->data << " ";
+
+      }
+      if (rightChild != nullptr) {
+        os << " " << rightChild->data << " ";
+      }
+    return os;
+  }
+
+};
+
 
 // do not edit below this line
 
